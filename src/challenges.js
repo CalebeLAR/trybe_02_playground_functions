a6972b30fd3998c30b7011e3fbd84d8e29023a89
CalebeLAR <ca6ele@gmail.com>
2022-07-02 22:52:11 -0400

    Requisito 10  testado e concluído

--- src/challenges.js
+++ src/challenges.js
@@ -1,6 +1,6 @@
 // Desafio 1 ==============================================================
 function compareTrue(valor1, valor2) {
-  // seu código aqui
+
   if (valor1 === true && valor2 === true) {
     return true;
   } else {
@@ -11,7 +11,7 @@ function compareTrue(valor1, valor2) {
 
 // Desafio 2 ==============================================================
 function calcArea(base, height) {
-  // seu código aqui
+
   area = (base * height) / 2;
   return area;
 
@@ -19,14 +19,14 @@ function calcArea(base, height) {
 
 // Desafio 3 ==============================================================
 function splitSentence(string) {
-  // seu código aqui
+
   let arrayDePalavras = string.split(' ', (string.length - 1));
   return arrayDePalavras;
 }
 
 // Desafio 4 ==============================================================
 function concatName(arrayDeStrings) {
-  // seu código aqui
+
   let ultimoIndice = arrayDeStrings.length - 1;
   let ultimoItem = arrayDeStrings[ultimoIndice];
   let primeiroItem = arrayDeStrings[0];
@@ -36,14 +36,14 @@ function concatName(arrayDeStrings) {
 
 // Desafio 5 ==============================================================
 function footballPoints(wins, ties) {
-  // seu código aqui
+
   let pontuação = (wins * 3 + ties * 1);
   return pontuação;
 }
 
 // Desafio 6 ==============================================================
 function highestCount(arrayDeNumeros) {
-  // seu código aqui
+
   let maior = 0;
   let contador = 0;
   let elemento;
@@ -70,7 +70,6 @@ function highestCount(arrayDeNumeros) {
 
 // Desafio 7 ==============================================================
 function catAndMouse(mouse, cat1, cat2) {
-  // seu código aqui
 
   let distânciaCat1 = mouse - cat1;
   let distânciaCat2 = mouse - cat2;
@@ -96,12 +95,12 @@ function catAndMouse(mouse, cat1, cat2) {
 
 // Desafio 8 ==============================================================
 function fizzBuzz(array) {
-  // seu código aqui
+
   let resposta = [];
   for (let indice = 0; indice < array.length; indice += 1) {
 
     let letraString = array[indice];
-    let condiçãoPorTres = (letraString % 3 === 0);   //vai sair false ou true
+    let condiçãoPorTres = (letraString % 3 === 0);   //Vai sair false ou true
     let condiçãoPorCinco = (letraString % 5 === 0);
 
     if (condiçãoPorTres === true && condiçãoPorCinco === true) {
@@ -120,7 +119,7 @@ function fizzBuzz(array) {
 
 // Desafio 9 ==============================================================
 function encode(string) {
-  // seu código aqui[
+
   let resposta = "";
   let vogais = ['a', 'e', 'i', 'o', 'u'];
   let numeros = ["1", "2", "3", "4", "5"];
@@ -131,7 +130,7 @@ function encode(string) {
       let letraVogais = vogais[indiceVogais];
 
       if (letraString === letraVogais) {
-        letraString = numeros[indiceVogais];     //a letra "o" está na posição 3 do array vogais, ele vai no array de numeros e pega o elemento na mesma posição: 3, e atribui a variavel letra. 
+        letraString = numeros[indiceVogais];     //A letra "o" está na posição 3 do array vogais, ele vai no array de numeros e pega o elemento na mesma posição: 3, e atribui a variavel letra. 
       }
     }
 
@@ -141,27 +140,45 @@ function encode(string) {
 }
 
 function decode(string) {
-  // seu código aqui
-    // seu código aqui
-    let resposta = "";
-    let regra = { "a": "1", "e": "2", "i": "3", "o": "4", "u": "5" }
-    for (let letra of string) {
 
-        for (let key in regra) {
-            if (letra === regra[key]) {
-                letra = key;
-            }
-        }
-    resposta += letra;   
+  let resposta = "";
+  let regra = { "a": "1", "e": "2", "i": "3", "o": "4", "u": "5" }
+  for (let letra of string) {
+
+    for (let key in regra) {
+      if (letra === regra[key]) {
+        letra = key;
+      }
     }
-    return resposta;
+    resposta += letra;
+  }
+  return resposta;
 }
 
 
 // Desafio 10 ==============================================================
-function techList(array, stryng) {
-  // seu código aqui
+function techList(array, string) {
 
+  if (array.length === 0 || string.length === 0) {
+    return "Vazio!";
+
+  } else {
+    let arrayOrdenado = array.sort()
+    let listaDeObjetos = [];
+    let indiceArray = 0
+
+    for (let tech of arrayOrdenado) {
+      let objeto = {};        // No início de toda repetição cria-se um objeto vazio, isso não deixa acumular elementos dentro dele.
+
+      objeto['tech'] = arrayOrdenado[indiceArray];
+      objeto['name'] = string;
+      listaDeObjetos.push(objeto);
+
+      indiceArray += 1;       // Ao final de toda repetição encrementa-se 1 ao indiceArray
+
+    }
+    return listaDeObjetos;
+  }
 }
 
 module.exports = {
