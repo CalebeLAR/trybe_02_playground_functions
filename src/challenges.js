5b5ebf13eebf2578deb96e5bbae929c40fbcc4cc
calebeLAR <ca6ele@gmail.com>
2024-12-21 14:16:46 -0300

    update

--- src/challenges.js
+++ src/challenges.js
@@ -1,32 +1,26 @@
 // Desafio 1 ==============================================================
 function compareTrue(valor1, valor2) {
-
   if (valor1 === true && valor2 === true) {
     return true;
   } else {
     return false;
   }
-
 }
 
 // Desafio 2 ==============================================================
 function calcArea(base, height) {
-
   area = (base * height) / 2;
   return area;
-
 }
 
 // Desafio 3 ==============================================================
 function splitSentence(string) {
-
-  let arrayDePalavras = string.split(' ', (string.length - 1));
+  let arrayDePalavras = string.split(' ', string.length - 1);
   return arrayDePalavras;
 }
 
 // Desafio 4 ==============================================================
 function concatName(arrayDeStrings) {
-
   let ultimoIndice = arrayDeStrings.length - 1;
   let ultimoItem = arrayDeStrings[ultimoIndice];
   let primeiroItem = arrayDeStrings[0];
@@ -36,14 +30,12 @@ function concatName(arrayDeStrings) {
 
 // Desafio 5 ==============================================================
 function footballPoints(wins, ties) {
-
-  let pontuação = (wins * 3 + ties * 1);
+  let pontuação = wins * 3 + ties * 1;
   return pontuação;
 }
 
 // Desafio 6 ==============================================================
 function highestCount(arrayDeNumeros) {
-
   let maior = 0;
   let contador = 0;
   let elemento;
@@ -53,7 +45,6 @@ function highestCount(arrayDeNumeros) {
       maior = elemento;
     } else if (elemento >= maior) {
       maior = elemento;
-
     }
   }
 
@@ -61,7 +52,6 @@ function highestCount(arrayDeNumeros) {
     elemento = arrayDeNumeros[index];
     if (maior === elemento) {
       contador += 1;
-
     }
   }
 
@@ -70,7 +60,6 @@ function highestCount(arrayDeNumeros) {
 
 // Desafio 7 ==============================================================
 function catAndMouse(mouse, cat1, cat2) {
-
   let distânciaCat1 = mouse - cat1;
   let distânciaCat2 = mouse - cat2;
   let resposta;
@@ -95,13 +84,11 @@ function catAndMouse(mouse, cat1, cat2) {
 
 // Desafio 8 ==============================================================
 function fizzBuzz(array) {
-
   let resposta = [];
   for (let indice = 0; indice < array.length; indice += 1) {
-
     let letraString = array[indice];
-    let condiçãoPorTres = (letraString % 3 === 0);   //Vai sair false ou true
-    let condiçãoPorCinco = (letraString % 5 === 0);
+    let condiçãoPorTres = letraString % 3 === 0; //Vai sair false ou true
+    let condiçãoPorCinco = letraString % 5 === 0;
 
     if (condiçãoPorTres === true && condiçãoPorCinco === true) {
       resposta.push('fizzBuzz');
@@ -112,25 +99,27 @@ function fizzBuzz(array) {
     } else {
       resposta.push('bug!');
     }
-
   }
   return resposta;
 }
 
 // Desafio 9 ==============================================================
 function encode(string) {
-
-  let resposta = "";
+  let resposta = '';
   let vogais = ['a', 'e', 'i', 'o', 'u'];
-  let numeros = ["1", "2", "3", "4", "5"];
+  let numeros = ['1', '2', '3', '4', '5'];
   for (let indiceString = 0; indiceString < string.length; indiceString += 1) {
     let letraString = string[indiceString];
 
-    for (let indiceVogais = 0; indiceVogais < vogais.length; indiceVogais += 1) {
+    for (
+      let indiceVogais = 0;
+      indiceVogais < vogais.length;
+      indiceVogais += 1
+    ) {
       let letraVogais = vogais[indiceVogais];
 
       if (letraString === letraVogais) {
-        letraString = numeros[indiceVogais];     //A letra "o" está na posição 3 do array vogais, ele vai no array de numeros e pega o elemento na mesma posição: 3, e atribui a variavel letra. 
+        letraString = numeros[indiceVogais]; //A letra "o" está na posição 3 do array vogais, ele vai no array de numeros e pega o elemento na mesma posição: 3, e atribui a variavel letra.
       }
     }
 
@@ -140,11 +129,9 @@ function encode(string) {
 }
 
 function decode(string) {
-
-  let resposta = "";
-  let regra = { "a": "1", "e": "2", "i": "3", "o": "4", "u": "5" }
+  let resposta = '';
+  let regra = { a: '1', e: '2', i: '3', o: '4', u: '5' };
   for (let letra of string) {
-
     for (let key in regra) {
       if (letra === regra[key]) {
         letra = key;
@@ -155,27 +142,23 @@ function decode(string) {
   return resposta;
 }
 
-
 // Desafio 10 ==============================================================
 function techList(array, string) {
-
   if (array.length === 0 || string.length === 0) {
-    return "Vazio!";
-
+    return 'Vazio!';
   } else {
-    let arrayOrdenado = array.sort()
+    let arrayOrdenado = array.sort();
     let listaDeObjetos = [];
-    let indiceArray = 0
+    let indiceArray = 0;
 
     for (let tech of arrayOrdenado) {
-      let objeto = {};        // No início de toda repetição cria-se um objeto vazio, isso não deixa acumular elementos dentro dele.
+      let objeto = {}; // No início de toda repetição cria-se um objeto vazio, isso não deixa acumular elementos dentro dele.
 
       objeto['tech'] = arrayOrdenado[indiceArray];
       objeto['name'] = string;
       listaDeObjetos.push(objeto);
 
-      indiceArray += 1;       // Ao final de toda repetição encrementa-se 1 ao indiceArray
-
+      indiceArray += 1; // Ao final de toda repetição encrementa-se 1 ao indiceArray
     }
     return listaDeObjetos;
   }
@@ -194,5 +177,3 @@ module.exports = {
   splitSentence,
   techList,
 };
-
-
--- src/challenges2.js
+++ src/challenges2.js
@@ -1,6 +1,5 @@
 // Desafio 11
 function generatePhoneNumber(array) {
-
   function verificaRepetições(array) {
     for (elementoParticular of array) {
       let cont = 0;
@@ -10,75 +9,94 @@ function generatePhoneNumber(array) {
         }
 
         if (cont >= 3) {
-          return false
+          return false;
         }
       }
     }
 
-    return true
+    return true;
   }
 
   function verificaNumeros(array) {
     for (num of array) {
       if (num >= 10 || num < 0) {
-        return false
+        return false;
       }
     }
   }
 
   function formataNumero(array) {
-    let string = ""
+    let string = '';
     for (let index = 0; index < array.length; index += 1) {
-      string += JSON.stringify(array[index])
+      string += JSON.stringify(array[index]);
     }
-    return "(" + string[0] + string[1] + ") " + string[2] + string[3] + string[4] + string[5] + string[6] + "-" + string[7] + string[8] + string[9] + string[10]
+    return (
+      '(' +
+      string[0] +
+      string[1] +
+      ') ' +
+      string[2] +
+      string[3] +
+      string[4] +
+      string[5] +
+      string[6] +
+      '-' +
+      string[7] +
+      string[8] +
+      string[9] +
+      string[10]
+    );
   }
-
 
   if (array.length !== 11) {
-    return "Array com tamanho incorreto."
-
-  } else if (verificaNumeros(array) === false || verificaRepetições(array) === false) {
-    return "não é possível gerar um número de telefone com esses valores";
-
+    return 'Array com tamanho incorreto.';
+  } else if (
+    verificaNumeros(array) === false ||
+    verificaRepetições(array) === false
+  ) {
+    return 'não é possível gerar um número de telefone com esses valores';
   } else {
-    return formataNumero(array)
+    return formataNumero(array);
   }
-
 }
 
 // Desafio 12
-function triangleCheck(lado_a,lado_b, lado_c) {
+function triangleCheck(lado_a, lado_b, lado_c) {
   // seu código aqui
-  
-  if (lado_a > (lado_b + lado_c) || lado_b > (lado_a + lado_c) || lado_c > (lado_a + lado_b)) {
-    return false;
 
-  } else if (lado_a < Math.abs(lado_b - lado_c) || lado_b < Math.abs(lado_a - lado_c) || lado_c < Math.abs(lado_a - lado_b)) {
+  if (
+    lado_a > lado_b + lado_c ||
+    lado_b > lado_a + lado_c ||
+    lado_c > lado_a + lado_b
+  ) {
     return false;
-
+  } else if (
+    lado_a < Math.abs(lado_b - lado_c) ||
+    lado_b < Math.abs(lado_a - lado_c) ||
+    lado_c < Math.abs(lado_a - lado_b)
+  ) {
+    return false;
   } else {
     return true;
   }
-
 }
 
 // Desafio 13
 function hydrate(pedido) {
   // seu código aqui
-  let numeroLetra = {"1":1 ,"2":2, "3":3, "4":4, "5":5, "6":6, "7":7, "8":8, "9":9}
+  let numeroLetra = { 1: 1, 2: 2, 3: 3, 4: 4, 5: 5, 6: 6, 7: 7, 8: 8, 9: 9 };
   let num_copos = 0;
   for (letra of pedido) {
     for (key in numeroLetra) {
-      if (letra === key ) {
+      if (letra === key) {
         num_copos += numeroLetra[key];
       }
     }
   }
   if (num_copos === 1) {
-    return JSON.stringify(num_copos) + " copo de água"
+    return JSON.stringify(num_copos) + ' copo de água';
   } else {
-    return JSON.stringify(num_copos) + " copos de água"
+    return JSON.stringify(num_copos) + ' copos de água';
   }
 }
 
